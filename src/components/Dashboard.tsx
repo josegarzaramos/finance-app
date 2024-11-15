0d1bc397c5af53fddef09aa7d6dca3c22750f322
Jose Luis Garza <josegarzaramos@gmail.com>
2024-11-15 00:19:21 -0600

    feat: add new components to dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,27 +1,28 @@
+import Budgets from './Budgets';
+import Pots from './Pots';
+import Summary from './Summary';
+import Card from './UI/Card';
+
 const Dashboard = () => {
   return (
-    <div className="flex flex-col px-10 py-8 lg:h-full lg:overflow-hidde">
-      <h1 className="text-4xl">Overview</h1>
+    <div className="flex flex-col gap-8 px-10 py-8 lg:h-full lg:overflow-hidde font-[family-name:var(--font-public-sans)]">
+      <h1 className="text-4xl font-bold">Overview</h1>
 
-      <div className="flex">
-        <p>Current Balance</p>
-        <p>Income</p>
-        <p>Expenses</p>
-      </div>
+      <Summary />
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 h-full [&_div]:bg-white [&_p]:p-3 grid-rows-4 min-h-0 [&_div]:rounded-2xl">
-        <div>
-          <p>Pots</p>
-        </div>
-        <div className="row-[span_2]">
-          <p>Budgets</p>
-        </div>
-        <div className="row-[span_3]">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-6 h-full grid-rows-[auto_1fr_1fr_1fr] min-h-0 [&>div]:p-6 [&>div]:bg-white">
+        <Card>
+          <Pots />
+        </Card>
+        <Card className="row-[span_2]">
+          <Budgets />
+        </Card>
+        <Card className="row-[span_3]">
           <p>Transactions</p>
-        </div>
-        <div className="row-[span_2]">
+        </Card>
+        <Card className="row-[span_2]">
           <p>Recurring Bills</p>
-        </div>
+        </Card>
       </div>
     </div>
   );
